140d10ecbaac22f1694797039af8c65ccd1cb72d
Glesko22 <vaintortoise89@gmail.com>
2025-03-19 18:26:49 -0400

    Added route to property listings page

--- tenanttalk/Frontend/src/App.js
+++ tenanttalk/Frontend/src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
+import Propertylisting from './pages/Propertylisting.tsx';
 import './App.css';
 
 function App() {
   return (
-    <>
-    <Home></Home>
-    </>
-)}
+    <Router>
+      <Routes>
+        {/* Renders the Home component at the root path "/" */}
+        <Route path="/" element={<Home />} />
+
+        {/* Renders the Propertylisting component at "/listings" */}
+        <Route path="/listings" element={<Propertylisting />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
