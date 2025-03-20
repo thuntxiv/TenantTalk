156b4210413d47c180ef69436f474da5e6ddd52c
Justin Isaac <kbulkiski@gmail.com>
2025-03-19 22:33:50 -0400

    Added location to property schema

--- tenanttalk/Backend/models/property.js
+++ tenanttalk/Backend/models/property.js
@@ -6,6 +6,7 @@ const propertySchema = new mongoose.Schema({
     landlordID: {type: mongoose.ObjectId },
     title: { type: String, required: true },
     address: { type: String, required: true },
+    location: { type: String, required: true },
     description: { type: String, required: true, unique: true },
     rent: { type: Number, required: true },
     type: { type: String, required: true },
